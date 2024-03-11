90f84fd89392c8baabe4961abf27d999c36093a8
Timothy Thijsen <t.thijsen@fontys.nl>
2024-03-11 09:52:12 +0100

    Adde IHotelDAL interface

---
+++ ReservaApp/LogicClasses/Interface/IHotelDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicClasses.Interface
+{
+    public interface IHotelDAL
+    {
+        void AddHotel(Hotel hotel);
+        void RemoveHotel(Hotel hotel);
+        void EditHotel(Hotel hotel);
+        Hotel GetHotelById(int id);
+        List<Hotel> GetAllHotels();
+    }
+}
