494d7259f453ad0402530bdca510af04fe39bfd5
Timothy Thijsen <t.thijsen@fontys.nl>
2024-03-12 19:49:53 +0100

    #14 added schedule class

---
+++ ReservaApp/LogicClasses/Interface/ISchedule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Interface
+{
+    public interface ISchedule
+
+    {
+        void AddReservation(Reservation reservation);
+        void RemoveReservation(Reservation reservation);
+        bool GetAvailability(DateRange dateRange);
+
+    }
+}
--- ReservaApp/LogicClasses/Schedule.cs
+++ ReservaApp/LogicClasses/Schedule.cs
@@ -1,6 +1,24 @@
-﻿namespace DomainLayer
+﻿using DomainLayer.Interface;
+
+namespace DomainLayer
 {
-    public class Schedule
+    public class Schedule : ISchedule
     {
+        List<Reservation> scheduledReservations = new List<Reservation> ();
+        public void AddReservation(Reservation reservation)
+        {
+            scheduledReservations.Add(reservation);
+        }
+
+        public bool GetAvailability(DateRange dateRange)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveReservation(Reservation reservation)
+        {
+            scheduledReservations.Remove(reservation);
+        }
+
     }
 }
