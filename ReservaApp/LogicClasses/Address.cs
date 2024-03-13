51accb65e3b3c73fd8ee0046f8826d00661363ce
Timothy Thijsen <t.thijsen@fontys.nl>
2024-03-13 10:53:55 +0100

    #18 Added Address class

---
+++ ReservaApp/LogicClasses/Address.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer
+{
+    public class Address
+    {
+        private string street, postalcode;
+        private int longitude , latitude;
+
+        public string Street { get { return street; } }
+        public string PostalCode { get {  return postalcode; } }
+
+        public Address (string street, string postalCode)
+        {
+            this.street = street;
+            this.postalcode = postalCode;
+        }
+    }
+}
