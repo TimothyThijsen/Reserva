cf8eb1f9042703c77828c2eb968213d56e19b510
Timothy Thijsen <t.thijsen@fontys.nl>
2024-03-18 11:55:01 +0100

    Added hotelmapper

---
+++ ReservaApp/DataAccesLayer/Mapper/HotelMapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer;
+using Microsoft.Data.SqlClient;
+
+namespace DataAccessLayer.Mapper
+{
+    public class HotelMapper
+    {
+        public Hotel GetHotel(SqlDataReader reader)
+        {
+            int id = reader.GetInt32(0);
+            string name = reader.GetString(1);
+            int cityId = reader.GetInt32(2);
+            int addressId = reader.GetInt32(3);
+            string street = reader.GetString(4);
+            string postalCode = reader.GetString(5);
+            string description = reader.GetString(6);
+
+            return new Hotel(id, name, description, cityId, new Address(street, postalCode));
+        }
+
+        public List<Hotel> GetHotels(SqlDataReader reader)
+        {
+            List<Hotel> hotels = new List<Hotel>();
+            while (reader.Read())
+            {
+                hotels.Add(GetHotel(reader));
+            }
+            return hotels;
+        }
+    }
+}
