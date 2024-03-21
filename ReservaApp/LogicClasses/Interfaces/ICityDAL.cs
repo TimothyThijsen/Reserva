685bf02361bafce106b5c6801b8297e81ec0b84d
Timothy Thijsen <t.thijsen@fontys.nl>
2024-03-21 11:31:39 +0100

    #25 added cityDAL class

---
+++ ReservaApp/LogicClasses/Interfaces/ICityDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Interfaces
+{
+    public interface ICityDAL
+    {
+        List<City> GetAllCities();
+        City GetCity(int cityId);
+        void AddCity(City city);
+        void UpdateCity(City city);
+        void RemoveCity(int id);
+    }
+}
