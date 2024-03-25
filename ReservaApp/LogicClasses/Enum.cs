ee60cd9ef60381c3c1ef14cbdb9f5f141687c922
Timothy Thijsen <t.thijsen@fontys.nl>
2024-03-25 17:35:10 +0100

    #10 added member class and added MemberType enum

---
+++ ReservaApp/LogicClasses/Enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer
+{
+    public enum MemberType
+    {
+        free_account,
+        premium_account,
+        owner_account
+    }
+}
---
+++ ReservaApp/LogicClasses/Member.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer
+{
+    public class Member : User
+    {
+        int? points;
+        bool verified;
+        MemberType memberType;
+        public int? Points { get { return points; } }
+        public MemberType MemberType {  get { return memberType; } }
+        public bool Verified { get { return verified; } }
+        public Member(string firstName, string lastName, string email, int age,MemberType memberType) : base(firstName, lastName, email, age)
+        {
+            this.memberType = memberType;
+            verified = false;
+        }
+        
+        public Member(int id, string firstName, string lastName, string email, int age, MemberType memberType, bool verified) : base(id, firstName, lastName, email, age)
+        {
+            this.memberType = memberType;
+            this.verified = verified;
+        }
+
+        public void VerifyAccount()
+        {
+            verified = true;
+        }
+    }
+}
