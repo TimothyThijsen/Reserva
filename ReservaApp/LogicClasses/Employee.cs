de573c041211db107292194237239ea7987d1253
Timothy Thijsen <t.thijsen@fontys.nl>
2024-03-25 21:39:53 +0100

    #27 added member manager class and IMemberDAL interface

---
+++ ReservaApp/LogicClasses/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer
+{
+    public class Employee : User
+    {
+        public Employee(string firstName, string lastName, string email, int age) : base(firstName, lastName, email, age)
+        {
+        }
+    }
+}
---
+++ ReservaApp/LogicClasses/Interfaces/IUserDAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Interfaces
+{
+    public interface IUserDAL
+    {
+        void AddUser(User user);
+        void RemoveUser(int id);
+        string[] GetCredentials(string email);
+        void ChangePassword(string oldPwd, string newPwd, int userId);
+        void EditUser(Member member);
+        User GetUser(int id);
+        List<Member> GetAllMembers();
+        List<Employee> GetAllEmployees();
+    }
+}
