a5f38c7c94098775a341382dcbe5d2ef74888c12
Timothy Thijsen <t.thijsen@fontys.nl>
2024-03-27 22:08:44 +0100

    Added model library to project

---
+++ ReservaApp/Models/Mapper/MemberModelExtension.cs
@@ -0,0 +1,22 @@
+﻿using DomainLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Mapper
+{
+	public static class MemberModelExtension
+	{
+
+		public static Member ToLogicLayer(this MemberModel memberModel)
+		{
+			Member member = new Member(memberModel.FirstName, memberModel.LastName, memberModel.Email, memberModel.Age, memberModel.Role);
+			member.Password = memberModel.Password;
+			return member;
+		}
+		
+
+	}
+}
---
+++ ReservaApp/Models/MemberModel.cs
@@ -0,0 +1,31 @@
+﻿using DomainLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+	public class MemberModel
+	{
+		[Required]
+		public string FirstName {  get; set; }
+		[Required]
+		public string LastName { get; set; }
+		//[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
+		[Range(18, 100, ErrorMessage = "Must be 18+")]
+		public int Age { get; set; }
+		[Required]
+		public MemberType Role { get; set; }
+
+		[StringLength(50, MinimumLength = 6, ErrorMessage = "Must be longer than 6 characters long.")]
+		[RegularExpression(@"^(?=.*\d).+$", ErrorMessage = "Password must contain at least one numeric digit.")]
+		public string Password {  get; set; }
+		public MemberModel() { }
+
+	}
+}
