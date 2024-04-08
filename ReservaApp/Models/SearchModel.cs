673de08497299cb671378b36376fe461597a75c2
Timothy Thijsen <t.thijsen@fontys.nl>
2024-04-08 14:10:19 +0200

    Code cleanup for searchbar partial class

--- ReservaApp/Models/SearchModel.cs
+++ ReservaApp/Models/SearchModel.cs
@@ -5,15 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using DomainLayer;
 
+
 namespace Models
 {
     public class SearchModel
     {
-        public int CityId {  get; set; }
-        public string StartDate { get; set; }
-        public string EndDate { get; set; }
-        public int AmountOfGuests { get; set; }
-        public List<City> Cities { get; set; }
-        public SearchModel() { }
+        public int? CityId {  get; set; }
+        public string? StartDate { get; set; }
+        public string? EndDate { get; set; }
+        public int? AmountOfGuests { get; set; }
+        public List<City>? Cities { get; set; }
+ 
     }
 }
