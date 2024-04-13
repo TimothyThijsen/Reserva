6946ef4fd3eab6b00be3d496b601d4fd19fafccb
Timothy Thijsen <t.thijsen@fontys.nl>
2024-04-13 21:25:32 +0200

    Added factory library

---
+++ ReservaApp/Factory/ReservationManagerFactory.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer;
+using DomainLayer.Interfaces;
+using DomainLayer.ServiceClasses;
+namespace Factory
+{
+    public  class ReservationManagerFactory
+    {
+        public static ReservationManager GetRoomReservationManager() 
+        {
+            return new ReservationManager(new RoomReservationDAL());
+        } 
+        public static ReservationManager GetActivityReservationManager() 
+        {
+            return new ReservationManager(new ActivityReservationDAL());
+        }
+    }
+}
